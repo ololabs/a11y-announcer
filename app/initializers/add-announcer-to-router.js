de0dd10e7ece0e463fbc82abcf42f46a5c87108c
Chris Krycho <chris@chriskrycho.com>
2017-06-19 10:48:34 -0400

    Cancel timer for `Ember.run.later` in initializer.
    This isn't the optimal fix: that's still to come, in the form of moving
    this out of an initializer and into `app/router.js` instead. However,
    this is a backwards-compatible stopgap which resolves a number
    of test failures which can arise if there are any bad interactions
    with other `Router` reopening or extension.

--- app/initializers/add-announcer-to-router.js
+++ app/initializers/add-announcer-to-router.js
@@ -10,14 +10,21 @@ export default {
       didTransition: function() {
         this._super(...arguments);
 
-        run.later(this, () => {
+        this._timerId = run.later(this, () => {
+          if (this.isDestroying || this.isDestroyed) { return; }
+
           let pageTitle = Ember.$('title').html().trim();
           let serviceMessage = this.get('announcer.message');
           let message = `${pageTitle} ${serviceMessage}`;
 
           this.get('announcer').announce(message, 'polite');
         }, 100);
-      }
+      },
+
+      willDestroy() {
+        run.cancel(this._timerId);
+        this._super();
+      },
     });
   }
 };
